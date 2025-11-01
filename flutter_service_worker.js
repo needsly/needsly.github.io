11f6c41b088d4aff0e736e6d5849e784e018e4d6
render1980 <render1980@gmail.com>
2025-11-01 22:07:12 +0200

    Release https://github.com/needsly/needsly/pull/1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -21,7 +21,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "canvaskit/canvaskit.js": "140ccb7d34d0a55065fbd422b843add6",
 "canvaskit/skwasm.wasm": "264db41426307cfc7fa44b95a7772109",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "4fa9849b1421b4bd9312290a3f239d9f",
+"main.dart.js": "a9b60b2daabd051f1cb90febdcbfaeee",
 "drift_worker.js": "afac8b57eb80f0846a382f7303929b0f",
 "version.json": "d719eadd803865d36d63873d78578b86",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
@@ -36,7 +36,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "drift_worker.dart": "eb8219d0cb3e6396dee4f89b6f833ecc",
 "index.html": "82b7036ac5a224256d91e394a95010dc",
 "/": "82b7036ac5a224256d91e394a95010dc",
-"flutter_bootstrap.js": "3afabb4c3350ed10f914fd264e637a7a",
+"flutter_bootstrap.js": "e9feef4bb39d6b4ceb717c89d4e88227",
 "sqlite3.wasm": "fa7637a49a0e434f2a98f9981856d118"};
 // The application shell files that are downloaded before a service worker can
 // start.
